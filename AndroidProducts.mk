6bcaf79897a2d65971355e8300a4809be7a438f2
JiangPan <xiaogoujiazu520@163.com>
2026-01-09 11:58:37 +0800

    device_tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_READBOY.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_READBOY-user \
+    omni_READBOY-userdebug \
+    omni_READBOY-eng
+
